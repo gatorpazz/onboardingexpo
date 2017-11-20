47ad53d9e9c006b0d26f3fa0473dbee8902bd076
Michael Pascuzzi <gatorpazz@gmail.com>
2017-11-19 21:22:39 -0500

    events and icon

--- src/assets/pages/Calendar/calendar.en.js
+++ src/assets/pages/Calendar/calendar.en.js
@@ -1,46 +1,83 @@
 export default calendarDates = {
-    '2017-08-08': {
+    '2017-11-17': {
+        marked: true,
+        events: [
+            {
+                startTime: '12:00PM',
+                endTime: '3:00PM',
+                text: 'United Way Jeremiah\'s Fundraiser',
+                location: 'Cafe'
+            }
+        ]
+    },
+    '2017-11-21': {
+        marked: true,
+        events: [
+            {
+                startTime: '5:30PM',
+                endTime: '6:30PM',
+                text: 'Zumba with Katrina',
+                location: 'Multipurpose Room'
+            }
+        ]
+    },
+    '2017-11-22': {
         marked: true,
         events: [
             {
                 startTime: '12:00PM',
                 endTime: '1:00PM',
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                text: 'Toastmasters',
+                location: 'Multipurpose Room'
             },
             {
-                startTime: '12:00PM',
-                endTime: '1:00PM',
-                text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-            },
-            {
-                startTime: '12:00PM',
-                endTime: '1:00PM',
-                text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                startTime: '5:00PM',
+                endTime: '5:45PM',
+                text: 'Chess Club',
+                location: 'Multipurpose Room'
             }
         ]
     },
-    '2017-08-09': {
+    '2017-11-23': {
         marked: true,
-        events: []
+        events: [
+            {
+                startTime: 'ALL DAY',
+                endTime: 'ALL DAY',
+                text: 'Thanksgiving',
+                location: ''
+            }
+        ]
     },
-    '2017-08-14': {
+    '2017-12-09': {
         marked: true,
-        events: []
+        events: [
+            {
+                startTime: '7:00PM',
+                endTime: '11:00PM',
+                text: 'The Express Scripts End of Year Gala',
+                location: 'The Ballroom at Church Street'
+            }
+        ]
     },
-    '2017-08-21': {
+    '2017-12-25': {
         marked: true,
-        events: []
+        events: [
+            {
+                startTime: 'ALL DAY',
+                endTime: 'ALL DAY',
+                text: 'Christmas'
+            }
+        ]
     },
-    '2017-08-22': {
+    '2017-12-31': {
         marked: true,
-        events: []
-    },
-    '2017-08-24': {
-        marked: true,
-        events: []
-    },
-    '2017-08-25': {
-        marked: true,
-        events: []
+        events: [
+            {
+                startTime: 'ALL DAY',
+                endTime: 'ALL DAY',
+                text: 'New Years Eve'
+            }
+        ]
     }
 };
